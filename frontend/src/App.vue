03e23d4f0661a1cd5afc5540bd8465a174fba1d6
smlee111 <vic_sm@naver.com>
2022-02-26 16:53:18 +0900

    kakaomap test1

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,9 +2,10 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="map" style="width:500px;height:400px;"></div>
   </div>
 </template>
-
+<script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=35535670248b973af25862ae7374977d"></script>
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
