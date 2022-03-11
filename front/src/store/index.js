a6e4f4444544a55b128ff120d2bb8ab8412918c3
lsh <jetaime12121@gmail.com>
2022-03-11 23:18:51 +0900

    story 가상 데이터

--- front/src/store/index.js
+++ front/src/store/index.js
@@ -7,13 +7,13 @@ export default new Vuex.Store({
   state: {
     items : [
       {
-         id : 1,
+         serialNum : 1,
          title : "신림 할리스", 
          x: 37.48445337260817,
          y: 126.9282442770891,
       },
       {
-        id : 2,
+        serialNum : 2,
         title : "부뚜막", 
         x: 37.484810395764505, 
         y: 126.92777209000958,
@@ -21,7 +21,41 @@ export default new Vuex.Store({
     ],
     nowLocation : {
       
-    }
+    },
+    storys : [
+      {
+        serialNum : 1,
+        cNum : 1,
+        id : 'lsh',
+        cont : '별로에요ㅜㅠ',
+        reg : '2022-03-11',
+        liked : 2
+      },
+      {
+        serialNum : 1,
+        cNum : 2,
+        id : 'lsm',
+        cont : '좋은데?',
+        reg : '2022-03-11',
+        liked : 3
+      },
+      {
+        serialNum : 2,
+        cNum : 3,
+        id : 'jyh',
+        cont : '시끄러워요',
+        reg : '2022-03-12',
+        liked : 4
+      },
+      {
+        serialNum : 2,
+        cNum : 4,
+        id : 'jyj',
+        cont : '조용해요',
+        reg : '2022-03-12',
+        liked : 5
+      },
+    ],
 
   },
   mutations: {
